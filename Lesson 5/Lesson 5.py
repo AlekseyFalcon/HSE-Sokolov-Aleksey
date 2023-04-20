8b55e7bfa2ddcfc04ea8dfc83e66189200a047ea
Donsokolov <donsokolov@rambler.ru>
2023-04-20 23:34:52 +0300

    Initial commit

--- Lesson 5/Lesson 5.py
+++ Lesson 5/Lesson 5.py
@@ -2,72 +2,47 @@ import json
 import csv
 import re
 
-"""
-Найдите информацию об организациях:
-a. Получите список ИНН из файла «traders.txt»;
-b. Найдите информацию об организациях с этими ИНН в файле
-«traders.json»;
-c. Сохраните информацию об ИНН, ОГРН и адресе организаций из файла
-«traders.txt» в файл «traders.csv».
-"""
-
 
 def task_1():
+    # Читаем все строки из файла "traders.txt" и создаем список
     with open("traders.txt", "r") as f:
-        inns = [i.rstrip() for i in f.readlines()]
+        inns = f.read().splitlines()
+
+    # Открываем файл "traders.json" и загружаем его содержимое в переменную all_traders
     with open("traders.json", "r") as f:
         all_traders = json.load(f)
-    traders = []
-    for trader in all_traders:
-        if trader['inn'] in inns:
-            traders.append(trader)
+
+    # Создаем список traders, содержащий только те элементы из all_traders, которые имеют inn из списка inns
+    traders = [trader for trader in all_traders if trader['inn'] in inns]
+
+    # Записываем данные из списка traders в файл "traders.csv"
     with open('traders.csv', 'w', newline='') as f:
         writer = csv.writer(f, delimiter=';')
         writer.writerow(['INN', 'OGRN', 'ADDRESS'])
-        for i in traders:
-            writer.writerow([i['inn'], i['ogrn'], i['address']])
-
-
-email_pattern = re.compile(r'\b[0-9a-zA-Z.-_]+@[0-9a-zA-Z.-_]+\.[a-zA-Z]+\b')
-inn_org_pattern = re.compile(r'\b\d{10}\b')
-inn_person_pattern = re.compile(r'\b\d{12}\b')
-
+        for trader in traders:
+            writer.writerow([trader['inn'], trader['ogrn'], trader['address']])
 
 def task_2():
+    # Открываем файл "1000_efrsb_messages.json" и загружаем его содержимое в переменную msgs
     with open("1000_efrsb_messages.json", "r") as f:
         msgs = json.load(f)
-    results = {}
-    for i in msgs:
-        emails = re.findall(email_pattern, i['msg_text'])
-        for email in emails:
-            email = email.lower()
-            searched = results.get(i['publisher_inn'])
-            if searched and email not in searched:
-                results[i['publisher_inn']].append(email)
-            elif not searched:
-                new_item = {i['publisher_inn']: [email]}
-                results.update(new_item)
-    inverted = {}
-    for inn, emails in results.items():
-        for email in emails:
-            searched = inverted.get(email)
-            if searched and inn not in searched:
-                inverted[email].append(inn)
-            elif not searched:
-                new_item = {email: [inn]}
-                inverted.update(new_item)
-    pass
 
+    # Создаем регулярное выражение для поиска email
+    email_pattern = re.compile(r'\b[0-9a-zA-Z.-_]+@[0-9a-zA-Z.-_]+\.[a-zA-Z]+\b')
+
+    # Создаем словарь, где ключи - email, а значения - список уникальных ИНН организаций, которые связаны с данным email.Все email приведены в нижний регистр.
+    result = {}
+    for msg in msgs:
+        emails = set(re.findall(email_pattern, msg['msg_text'].lower()))
+        for email in emails:
+            inn = msg['publisher_inn']
+            result.setdefault(email, []).append(inn)
+
+    # Записываем результаты в файл "emails.json"
     with open('emails.json', "w") as f:
-        json.dump(results, f)
-    print("stop")
+        json.dump(result, f)
 
 
-def test():
-    task_1()
-    task_2()
-
-
-if __name__ == "__main__":
-    test()
-    print("stop")
+# Вызываем функции
+task_1()
+task_2()
